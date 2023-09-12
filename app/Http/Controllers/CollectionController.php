67caf3f554b114cb28be7de79de5a1a571b9f35c
thomasbrian4545 <thomasbrian4545@gmail.com>
2023-09-12 21:49:40 +0700

    Collection dari berbagai tipe data

--- app/Http/Controllers/CollectionController.php
+++ app/Http/Controllers/CollectionController.php
@@ -19,10 +19,16 @@ class CollectionController extends Controller
     public function collectionDua()
     {
         $collection = collect([1, 9, 3, 4, 5, 3, 5, 7]);
-        echo $collection[2]; echo "<br>";
+        echo $collection[2];
+        echo "<br>";
         foreach ($collection as $value) {
             echo "$value ";
         }
+        echo "<br>";
+        // Collection dari berbagai tipe data
+        $collection = collect(["Mawar", "Anggrek", "Dahlia", 5, 7, 9]);
+        echo $collection;
+        echo "<br>";
     }
 
     public function collectionTiga()
