8d6ba8f81b1a3ca4b4bf9f1cc13b56cdafc77c7f
thomasbrian4545 <thomasbrian4545@gmail.com>
2023-09-12 21:31:06 +0700

    Helper function collect()

--- app/Http/Controllers/CollectionController.php
+++ app/Http/Controllers/CollectionController.php
@@ -8,7 +8,11 @@ class CollectionController extends Controller
 {
     public function collectionSatu()
     {
-
+        $collection = collect([1, 9, 3, 4, 5, 3, 5, 7]);
+        echo "<pre>";
+        var_dump($collection);
+        echo "</pre>";
+        echo $collection;
     }
 
     public function collectionDua()
